f90f923c22c87070da7b2b02016dc3ca985f9314
Hassaan <hassaan.aimviz@gmail.com>
2019-02-15 15:03:07 +0500

    Hassaan

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Container, Row, Col, Button, Input, Label, FormGroup, ListGroup, ListGroupItem
+} from 'reactstrap';
+import { create, read, getById, deleteFirebase } from './config/Firebase';
+import swal from 'sweetalert';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      itemVal: '',
+      list: [],
+      itemId: '',
+      color: "success",
+      text: "Add"
+    }
+
+    this.createData = this.createData.bind(this);
+  }
+
+  createData() {
+    const { itemVal, itemId } = this.state;
+    let id = "";
+    let obj = {};
+    if (itemId == '') {
+      id = Math.floor(Math.random() * 123456789);
+      obj = { id: id.toString(), item: itemVal };
+    }
+    else{
+      id = itemId;
+      obj = { id: id.toString(), item: itemVal };
+    }
+    create(obj).then((resp)=>{
+      if(resp == "Done"){
+        read().then((res) => {
+          this.setState({
+            list: res,
+            itemVal: '',
+            itemId : '',
+            color: "success",
+            text: "Add"
+          });
+        });
+      }
+    });
+    
+  }
+
+  updateData(e) {
+    const id = e.target.value;
+    getById(id).then((res) => {
+      this.setState({
+        itemVal: res.item,
+        itemId: res.id,
+        color: "primary",
+        text: "Update"
+      })
+    });
+  }
+
+  deleteData(e){
+    const id = e.target.value;
+    deleteFirebase(id).then((response)=>{
+      
+      this.setState({
+        list: response
+      });
+      
+    });
+  }
+
+  componentDidMount() {
+    read().then((res) => {
+      this.setState({
+        list: res
+      });
+    });
+  }
+
   render() {
+    const { list, itemVal, color, text } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Row className="mt-2">
+          <Col xs="6">
+            <FormGroup>
+              <Label>Item:</Label>
+              <Input value={itemVal} placeholder="Item..." onChange={((e) => { this.setState({ itemVal: e.target.value }) })} />
+            </FormGroup>
+            <FormGroup>
+              <Button onClick={this.createData} color={color}>{text}</Button>
+            </FormGroup>
+          </Col>
+          <Col xs="6">
+            <FormGroup>
+              <Label>List:</Label>
+              {list.length > 0 && <ListGroup>
+                {list.map((elem) => {
+                  return <ListGroupItem key={elem.id}>
+                    <div className="d-flex justify-content-between">
+                      <div>
+                        <span className="justify-content-start">{elem.item}</span>
+                      </div>
+                      <div>
+                        <Button value={elem.id} onClick={this.updateData.bind(this)} className="mr-2 justify-content-end" color="info" size="sm">Edit</Button>
+                        <Button value={elem.id} onClick={this.deleteData.bind(this)} className="justify-content-end" color="danger" size="sm">Delete</Button>
+                      </div>
+                    </div>
+                  </ListGroupItem>
+                })}
+
+              </ListGroup>}
+              {list.length == 0 && <ListGroup>
+                <ListGroupItem>
+                    <div className="d-flex justify-content-between">
+                      <div>
+                        <span className="justify-content-start">No Data Found!</span>
+                      </div>
+                    </div>
+                  </ListGroupItem>
+              </ListGroup>
+              }
+            </FormGroup>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
---
+++ src/config/Firebase.js
@@ -0,0 +1,102 @@
+import * as firebase from 'firebase';
+import swal from 'sweetalert';
+
+const config = {
+    apiKey: "AIzaSyAHdK_aCAVU6ov8INcoO-Aa9OAxb1beaSU",
+    authDomain: "hassaan-feb-12-2019.firebaseapp.com",
+    databaseURL: "https://hassaan-feb-12-2019.firebaseio.com",
+    projectId: "hassaan-feb-12-2019",
+    storageBucket: "hassaan-feb-12-2019.appspot.com",
+    messagingSenderId: "505333331082"
+};
+firebase.initializeApp(config);
+
+var db = firebase.firestore();
+
+function create(obj) {
+    return new Promise((reso, reje)=>{
+        getById(obj.id).then((res) => {
+            if (res !== "No such document!") {
+                db.collection("myList").doc(obj.id).update(obj)
+                    .then(() => {
+                        //swal("Success", "Save Successfully..", "success");
+                        reso("Done");
+                    })
+                    .catch((err) => {
+                        swal("Error", err, "error");
+                    })
+            }
+            else {
+                db.collection("myList").doc(obj.id).set(obj)
+                    .then(() => {
+                        //swal("Success", "Save Successfully..", "success");
+                        reso("Done");
+                    })
+                    .catch((err) => {
+                        swal("Error", err, "error");
+                    })
+            }
+        })
+    })
+    
+
+}
+
+function read() {
+
+    return new Promise((res, rej) => {
+        var myList = [];
+        db.collection("myList").get().then(function (querySnapshot) {
+            console.log(querySnapshot);
+            if(querySnapshot.docs.length == 0){
+                res(myList);
+            }
+            else{
+                querySnapshot.forEach(function (doc) {
+                    // doc.data() is never undefined for query doc snapshots
+                    //console.log(doc.id, " => ", doc.data());
+                    myList.push(doc.data());
+                    res(myList);
+                });
+            }
+            
+        });
+        
+    })
+
+}
+
+function getById(id) {
+    return new Promise((res, rej) => {
+        var docRef = db.collection("myList").doc(id);
+        docRef.get().then(function (doc) {
+            if (doc.exists) {
+                res(doc.data());
+            } else {
+                res("No such document!");
+            }
+        }).catch(function (error) {
+            console.log("Error getting document:", error);
+        });
+    })
+
+}
+
+function deleteFirebase(id) {
+    return new Promise((res, rej) => {
+        db.collection("myList").doc(id).delete().then(function () {
+            console.log("Delete from firebase.js");
+            read().then((resp)=>{
+                console.log(resp);
+                res(resp);
+                
+                
+            })
+        }).catch(function (error) {
+            console.error("Error removing document: ", error);
+        });
+    })
+
+}
+
+export { create, read, getById, deleteFirebase };
